318e7bbaa7c659f8c919baafe2d4c506026923b4
Sahar Jalili <153284020+SaharJalili236@users.noreply.github.com>
2026-05-13 16:13:57 +0200

    Add weather checking functionality in script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const apiKey = "863242cfb2b1d357e6093d9a4df19a4b";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    
+    if(response.status == 404) {
+        alert("City not found!");
+    } else {
+        var data = await response.json();
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    }
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
